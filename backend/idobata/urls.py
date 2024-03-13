b7c89d5afb54a3efb0f84d9f180b7b3f4dd27989
Tayu <yukitake844@gmail.com>
2024-03-14 00:03:01 +0900

    backend api

--- backend/idobata/urls.py
+++ backend/idobata/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import TemplateView
+from .views import TemplateView, RoomListCreateAPIView, MessageListCreateAPIView
 
 urlpatterns = [
-    path("", TemplateView.as_view(), name="index")
+    path("", TemplateView.as_view(), name="index"),
+    path('rooms/', RoomListCreateAPIView.as_view(), name='room-list-create'),
+    path('messages/', MessageListCreateAPIView.as_view(), name='message-list-create'),
 ]
